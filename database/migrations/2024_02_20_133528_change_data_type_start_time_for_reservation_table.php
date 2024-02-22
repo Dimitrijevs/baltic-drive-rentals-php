b63a1d80c4c13fe4fd4f4bfce24a4383fa0db831
Dimitrijevs <githubodinlv@gmail.com>
2024-02-22 21:17:07 +0200

    Add files via upload

---
+++ database/migrations/2024_02_20_133528_change_data_type_start_time_for_reservation_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            // for column start_date change data type to time
+            $table->time('start_time')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            // for column start_date change data type to date
+            $table->date('start_time')->change();
+        });
+    }
+};
